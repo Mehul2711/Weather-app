1cfc0e782bd49fb15e517a06b694aed59607a31a
mehul2711 <mehul271102@gmail.com>
2023-12-26 11:39:57 +0530

    :ambulance: Update

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
 
   const apiKey = process.env.NEXT_PUBLIC_WEATHER_KEY;
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   console.log(city);
 
   const fetchWeather = (e) => {
